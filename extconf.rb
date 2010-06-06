06b4c77948f3a6b398b0be2ac3f516f5bbbf3f8e
tadashi <tadashi@8fd9d052-9019-0410-b429-c1b3abf8354a>
2010-06-06 14:46:30 +0000

    add a comment to clarify the aim of 'have_header'.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/decimal/trunk@104 8fd9d052-9019-0410-b429-c1b3abf8354a

--- extconf.rb
+++ extconf.rb
@@ -1,7 +1,7 @@
 require "mkmf"
 
 (cflags = arg_config("--cflags")) && $CFLAGS << " #{cflags}"
-have_header "ruby/ruby.h"
+have_header "ruby/ruby.h" # to distiguish Ruby 1.8 from 1.9 with <version.h>
 have_func "rb_big_div"
 have_func "rb_big_modulo"
 
