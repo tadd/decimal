b95aa179f9114e34605f9a0f0984b70378f8cbb4
tadashi <tadashi@8fd9d052-9019-0410-b429-c1b3abf8354a>
2010-07-11 04:55:37 +0000

    add e(), pi(), atan(), acos(), asin().
    git-svn-id: svn+ssh://rubyforge.org/var/svn/decimal/trunk@133 8fd9d052-9019-0410-b429-c1b3abf8354a

--- lib/decimal/math.rb
+++ lib/decimal/math.rb
@@ -15,10 +15,12 @@ module Decimal::Math
       t = x.divide(s, scale+1, :down) + s
       s = t.divide(2, scale+1, :down)
     end while s < last
-    last
+    last.round(scale, rounding)
   end
 
-  # copied from BigDecimal
+  # functions below are copied from BigDecimal:
+  #   sin, cos, exp, log, atan, pi
+
   def sin(x, scale, rounding=:down)
     x = Decimal(x) if x.integer?
     return Decimal::NAN if x.infinite? or x.nan?
@@ -42,7 +44,6 @@ module Decimal::Math
     y.round(scale, rounding)
   end
 
-  # copied from BigDecimal
   def cos(x, scale, rounding=:down)
     x = Decimal(x) if x.integer?
     return Decimal::NAN if x.infinite? or x.nan?
@@ -66,11 +67,14 @@ module Decimal::Math
   end
 
   def tan(x, scale, rounding=:down)
+    x = Decimal(x) if x.integer?
+    return Decimal::NAN if x.infinite? or x.nan?
+    return Decimal("0e#{-scale}") if x.zero?
+    raise Errno::EDOM if x.infinity? # XXX
     s, c = sin(x, scale+1), cos(x, scale+1)
     s.divide(c, scale, rounding)
   end
 
-  # copied from BigDecimal
   def exp(x, scale, rounding=:down)
     x = Decimal(x) if x.integer?
     return Decimal::NAN if x.infinite? or x.nan?
@@ -96,7 +100,10 @@ module Decimal::Math
     end
   end
 
-  # copied from BigDecimal
+  def e(scale, rounding=:down)
+    exp(1, scale, rounding)
+  end
+
   def log(x, scale, rounding=:down)
     x = Decimal(x) if x.integer?
     return Decimal::NAN if x.nan?
@@ -115,6 +122,80 @@ module Decimal::Math
       break if d.zero?
       y += d
     end
-    y * 2
+    (y * 2).round(scale, rounding)
+  end
+
+  def pi(scale, rounding=:down)
+    pi = 0
+
+    k = 1
+    t = Decimal(-80)
+    u = nil
+    loop do
+      t = t.divide(-25, scale+1, :down) 
+      u = t.divide(k, scale+1, :down)
+      break if u.zero?
+      pi += u
+      k += 2
+    end
+
+    k = 1
+    t = Decimal(956)
+    loop do
+      t = t.divide(-57121, scale+1, :down)
+      u = t.divide(k, scale+1, :down)
+      break if u.zero?
+      pi += u
+      k += 2
+    end
+    pi.round(scale, rounding)
+  end
+
+  def atan(x, scale, rounding=:down)
+    x = Decimal(x) if x.integer?
+    return Decimal::NAN if x.nan?
+    return Decimal("0e#{-scale}") if x.zero?
+    x = -x if negative = x < 0
+    local_pi = lambda{pi(scale+1, :down)}
+    return local_pi[].divide(negative ? -2 : 2, scale, rounding) if x.infinite?
+    return local_pi[].divide(negative ? -4 : 4, scale, rounding) if x == 1
+    x = Decimal(1).divide(x, scale+1, :down) if inverse = x > 1
+    if double = x > Decimal("0.5")
+      inx = -1 + sqrt(1 + x * x, scale+1, :down)
+      x = inx.divide(x, scale+1,:down)
+    end
+    t = d = y = x
+    r = 3
+    x2 = (x * x).floor(scale+1)
+    loop do
+      t = ((-t) * x2).floor(scale+1)
+      d = t.divide(r, scale+1, :down)
+      break if d.zero?
+      y += d
+      r += 2
+    end
+    y *= 2 if double
+    y = local_pi[].divide(2, scale+1, :down) - y if inverse
+    y = -y if negative
+    y.round(scale, rounding)
+  end
+
+  def asin(x, scale, rounding=:down)
+    x = Decimal(x) if x.integer?
+    return Decimal::NAN if x.nan?
+    return Decimal("0e#{-scale}") if x.zero?
+    raise Errno::EDOM if x > 1 or x < -1 # XXX
+    x2 = sqrt(1 - x * x, scale+1, :down)
+    atan(x.divide(x2, scale+1, :down), scale, rounding)
+  end
+  
+  def acos(x, scale, rounding=:down)
+    x = Decimal(x) if x.integer?
+    return Decimal::NAN if x.nan?
+    return Decimal("0e#{-scale}") if x == 1
+    raise Errno::EDOM if x.infinite? or x > 1 or x < -1 # XXX
+    pi_half = pi(scale+1, :down).divide(2, scale+1, :down)
+    as = asin(x, scale+1, :down)
+    (pi_half - as).round(scale, rounding)
   end
 end
