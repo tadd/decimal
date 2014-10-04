7cb98c6169647ff974ca9abbd81f3d5d2a159e64
Tadashi Saito <tad.a.digger@gmail.com>
2014-10-04 12:48:19 +0900

    support Ruby 2.0.0

--- extconf.rb
+++ extconf.rb
@@ -2,7 +2,11 @@ require "mkmf"
 
 cflags = arg_config("--cflags")
 $CFLAGS << " #{cflags}" if cflags
-version = '193'
+version = if have_func('rb_gc_mark_threads')
+            '193'
+          else
+            '200'
+          end
 $CFLAGS << " -DINUM_SOURCE_FILE=" + %(\\"inum#{version}.h\\")
 
 create_makefile "decimal"
