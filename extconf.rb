bee9f15f204efb217470e227030dd131c1616616
Tadashi Saito <tad.a.digger@gmail.com>
2014-10-13 23:36:09 +0900

    support trunk, aka Ruby 2.2

--- extconf.rb
+++ extconf.rb
@@ -6,8 +6,10 @@ version = if have_func('rb_gc_mark_threads')
             '193'
           elsif have_func('rb_f_lambda')
             '200'
-          else
+          elsif have_func('rb_fork')
             '21'
+          else
+            '22'
           end
 $CFLAGS << " -DINUM_SOURCE_FILE=" + %(\\"inum#{version}.h\\")
 
